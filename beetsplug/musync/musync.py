b5053bd07411d633aa9a1b35c595389c8123952e
whatphilipcodes <gerdes.philip@gmail.com>
2024-09-26 22:50:59 +0200

    fx: test setup

--- beetsplug/musync/musync.py
+++ beetsplug/musync/musync.py
@@ -1,22 +1,10 @@
-from beets import plugins, IncludeLazyConfig
-
-from typing import Any, Dict
-
-
-# config setup
-JSONDict = Dict[str, Any]
-DEFAULT_CONFIG: JSONDict = {
-    "add_to_music_path": "~/Music/Music/Media/Automatically Add to Music.localized"
-}
+from beets import plugins
 
 
 class Musync(plugins.BeetsPlugin):
-    beets_config: IncludeLazyConfig
-
     def __init__(self):
         super().__init__()
-        self.config.add(DEFAULT_CONFIG.copy())
-        self.register_listener("import_task_end", self.sync_task)
+        self.register_listener("pluginload", self.loaded)
 
-    def sync_task(self):
-        self._log.info("after import")
+    def loaded(self):
+        self._log.info("Plugin loaded!")
